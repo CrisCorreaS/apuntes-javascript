7be7d83ceb0b8fa7149e4ca71895cb034d7fb817
Cris Correa <cristina.correa.segade@gmail.com>
2023-09-19 11:21:47 +0200

    commit ejercicios tema 2

---
+++ Ejercicios/Unidad 2/Ejercicio 2/script.js
@@ -0,0 +1,9 @@
+let cadena1 = "Cristina";
+let cadena2 = "Correa";
+let numero1 = 23;
+let numero2 = 2000;
+
+alert("Este es el primer 'alert'");
+alert(cadena1 + "\n" + cadena2);
+alert(numero1 + numero2);
+alert(cadena1 + cadena2 + numero1 + numero2);
---
+++ Ejercicios/Unidad 2/Ejercicio 3/script.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = "Laura";
+let z = "123";
+
+alert("La operación 10 == 10 es " + (x == 10));
+alert("La operación 10 === 10 es " + (x === 10));
+alert("La operación 10 === 10.0 es " + (x === 10.0));
+alert("La operación “Laura” == “laura” es " + (y == "laura"));
+alert("La operación “Laura” > “laura” es " + (y > "laura"));
+alert("La operación “Laura” < “laura” es " + (y < "laura"));
+alert("La operación “123” == 123 es " + (z == 123));
+alert("La operación “123” === 123 es " + (z === 123));
+alert("La operación parseInt(“123”) === 123 es " + (parseInt(z) === 123));
