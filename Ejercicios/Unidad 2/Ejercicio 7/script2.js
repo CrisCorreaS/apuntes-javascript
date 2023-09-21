65e6789c6cebf03980b8403c8bfba8e75744a883
Cris Correa <cristina.correa.segade@gmail.com>
2023-09-21 14:19:21 +0200

    commit ejercicio 7 tema 2

---
+++ Ejercicios/Unidad 2/Ejercicio 7/script2.js
@@ -0,0 +1,8 @@
+const respuestaCorrecta = "Suárez";
+
+let respuestaUsuario = prompt("¿Cuál es el apellido del primer presidente de la democracia española?");
+
+while(respuestaUsuario != respuestaCorrecta){
+    alert("La respuesta es incorrecta. Intente de nuevo.");
+    respuestaUsuario = prompt("¿Cuál es el apellido del primer presidente de la democracia española?");
+}
