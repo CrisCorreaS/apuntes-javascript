a9ef7776f2e51d63f09f88a4b5f28ba348744c8f
Cris Correa <cristina.correa.segade@gmail.com>
2023-09-28 13:24:02 +0200

    commit apartado 2 tarea unidad 2

---
+++ Ejercicios/Unidad 2/Tarea/Apartado 2/script.js
@@ -0,0 +1,11 @@
+let resultado1 = 125 >> 3;
+let resultado2 = 40 << 2;
+let resultado3 = 25 >> 1;
+let resultado4 = 10 << 4;
+
+document.write("<ul>");
+document.write("<li>125 / 8 = " + resultado1 + "</li>");
+document.write("<li>40 x 4 = " + resultado2 + "</li>");
+document.write("<li>25 / 2 = " + resultado3 + "</li>");
+document.write("<li>10 x 16 = " + resultado4 + "</li>");
+document.write("</ul>");
