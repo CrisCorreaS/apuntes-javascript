5b245f35a2b76b5683e94985cdccc96ef434f592
Cris Correa <cristina.correa.segade@gmail.com>
2023-10-03 10:56:18 +0200

    commit mejora script ejercicio 2 tema 3

--- Ejercicios/Unidad 3/Ejercicio 2/script2.js
+++ Ejercicios/Unidad 3/Ejercicio 2/script2.js
@@ -6,11 +6,11 @@ let mesCumpleaños = prompt("Escribe el mes de tu cumpleaños");
 let texto = "Los años en los que tu cumpleaños cae en domingo son: ";
 let year = new Date().getFullYear();
 
-for(year; year<=2100; year++){
-    let fecha = new Date(year,mesCumpleaños-1,diaCumpleaños);
-    if(fecha.getDay() == 0){
-        texto += fecha.getFullYear()+", ";
-    }
+for (year; year <= 2100; year++) {
+  let fecha = new Date(year, mesCumpleaños - 1, diaCumpleaños);
+  if (fecha.getDay() == 0) {
+    texto += year + ", ";
+  }
 }
 
 alert(texto);
