31d581b5f87226cb265218869ffc47c96008f0bf
Cris Correa <cristina.correa.segade@gmail.com>
2023-10-06 14:18:23 +0200

    commit ejercicio 7 unidad 3

--- Ejercicios/Unidad 3/Ejercicio 7/script2.js
+++ Ejercicios/Unidad 3/Ejercicio 7/script2.js
@@ -1,12 +1,16 @@
 //Cristina Correa
 
-setTimeout(function () {
+setTimeout(showTime(), 2000);
+
+let intervalID = setInterval(showTimeStop(), 2000);
+
+function showTime() {
   let fechaActual = new Date();
   document.getElementById("reloj1").innerHTML = fechaActual;
-}, 2000);
+}
 
-let intervalID = setInterval(function () {
+function showTimeStop() {
   let fechaActual = new Date();
-  document.getElementById("reloj2").innerHTML =fechaActual;
+  document.getElementById("reloj2").innerHTML = fechaActual;
   clearInterval(intervalID); // Detenemos el intervalo después de la primera ejecución
-}, 2000);
+}
