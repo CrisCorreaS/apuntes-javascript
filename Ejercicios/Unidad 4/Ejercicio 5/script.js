51043b2c944f4c830bab4ae83cc55a49fdeb3c96
Cris Correa <cristina.correa.segade@gmail.com>
2023-10-25 14:31:48 +0200

    commit ejercicio 5 unidad 4

---
+++ Ejercicios/Unidad 4/Ejercicio 5/script.js
@@ -0,0 +1,11 @@
+// Cristina Correa 
+
+let fuerza = 1;
+
+function aumentar(){
+    fuerza ++;
+}
+
+function mostrar(){
+    document.getElementById("respuesta").innerHTML = "La fuerza del jugador es " + fuerza;
+}
