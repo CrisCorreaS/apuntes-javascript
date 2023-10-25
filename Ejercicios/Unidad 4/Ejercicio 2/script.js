83be23aab3af219788c428e31784459432eb4d14
Cris Correa <cristina.correa.segade@gmail.com>
2023-10-25 13:40:00 +0200

    commit ejercicio 2 unidad 4

---
+++ Ejercicios/Unidad 4/Ejercicio 2/script.js
@@ -0,0 +1,57 @@
+// Cristina Correa
+
+let letrasDNI = [
+  "T",
+  "R",
+  "W",
+  "A",
+  "G",
+  "M",
+  "Y",
+  "F",
+  "P",
+  "D",
+  "X",
+  "B",
+  "N",
+  "J",
+  "Z",
+  "S",
+  "Q",
+  "V",
+  "H",
+  "L",
+  "C",
+  "K",
+  "E",
+];
+let dnis = [];
+
+window.addEventListener("load", inicio, true);
+
+function inicio() {
+  document.getElementById("salida");
+
+  document.getElementById("boton").addEventListener("click", ejecuta, true);
+}
+
+function ejecuta() {
+  let letra = document.getElementById("letra").value;
+  let salida = document.getElementById("salida");
+
+  let posicion = letrasDNI.indexOf(letra);
+
+  for (let cont = 1; cont <= 999; cont++) {
+    if (Number.parseInt(cont) % 23 == posicion) {
+      if (cont < 10) {
+        dnis.push("00" + cont);
+      } else if (cont < 100) {
+        dnis.push("0" + cont);
+      } else {
+        dnis.push(cont);
+      }
+    }
+  }
+
+  salida.innerHTML = "Numero de dnis: " + dnis.toString();
+}
