ddae93d55a3d30629100b797a0e5ae64a85b2a8c
Cristina Correa <cristina.correa.segade@gmail.com>
2023-11-02 17:49:11 +0100

    commit tarea unidad 4

--- Ejercicios/Unidad 4/Tarea/edificio.js
+++ Ejercicios/Unidad 4/Tarea/edificio.js
@@ -1,11 +1,63 @@
 // Cristina Correa
 
 export default class Edificio {
-  constructor(calle, numero, codigoPostal, plantasEdificio) {
+  constructor(calle, numero, codigoPostal) {
     this.calle = calle;
     this.numero = numero;
     this.codigoPostal = codigoPostal;
-    this.plantasEdificio = plantasEdificio;
+    this.plantas = [];
+    console.log(
+      `Construido nuevo edificio en calle: ${this.calle}, nº: ${this.numero}, CP: ${this.codigoPostal}.`
+    );
+  }
+
+  agregarPlantasYPuertas(numPlantas, numPuertas) {
+    for (let i = 0; i < numPlantas; i++) {
+      this.plantas.push(new Array(numPuertas).fill(null));
+    }
+  }
+
+  modificarNumero(numero) {
+    this.numero = numero;
+  }
+
+  modificarCalle(calle) {
+    this.calle = calle;
+  }
+
+  modificarCodigoPostal(codigo) {
+    this.codigoPostal = codigo;
+  }
+
+  imprimeCalle() {
+    return this.calle;
+  }
+
+  imprimeNumero() {
+    return this.numero;
+  }
+
+  imprimeCodigoPostal() {
+    return this.codigoPostal;
+  }
+
+  agregarPropietario(nombre, planta, puerta) {
+    this.plantas[planta - 1][puerta - 1] = nombre;
+    console.log(
+      `${nombre} es ahora el propietario de la puerta ${puerta} de la planta ${planta}.`
+    );
+  }
+
+  imprimePlantas() {
+    this.plantas.forEach((planta, i) => {
+      planta.forEach((puerta, j) => {
+        console.log(
+          `Propietario del piso ${j + 1} de la planta ${i + 1}: ${
+            puerta ? puerta : ""
+          }`
+        );
+      });
+    });
   }
 }
 export { Edificio };
--- Ejercicios/Unidad 4/Tarea/script.js
+++ Ejercicios/Unidad 4/Tarea/script.js
@@ -1,7 +1,32 @@
 // Cristina Correa
 
-/*
-Se puede hacer con clases o con arrays, si es con array sería:
-    edificioA[0][1] -> planta 0 puerta 1
-    ...
-*/
+import Edificio from "./edificio.js";
+
+let edificioA = new Edificio("Garcia Prieto", 58, 15706);
+let edificioB = new Edificio("Camino Caneiro", 29, 32004);
+let edificioC = new Edificio("San Clemente", "s/n", 15705);
+
+console.log(
+  `El código postal del edificio A es: ${edificioA.imprimeCodigoPostal()}`
+);
+
+console.log(`La calle del edificio C es: ${edificioC.imprimeCalle()}`);
+
+console.log(
+  `El edificio B está situado en la calle ${edificioB.imprimeCalle()} número ${edificioB.imprimeNumero()}`
+);
+
+edificioA.agregarPlantasYPuertas(2, 3);
+
+edificioA.agregarPropietario("Jose Antonio Lopez", 1, 1);
+edificioA.agregarPropietario("Luisa Martinez", 1, 2);
+edificioA.agregarPropietario("Marta Castellón", 1, 3);
+edificioA.agregarPropietario("Antonio Pereira", 2, 2);
+
+edificioA.imprimePlantas();
+
+edificioA.agregarPlantasYPuertas(1, 3);
+
+edificioA.agregarPropietario("Pedro Meijide", 3, 2);
+
+edificioA.imprimePlantas();
