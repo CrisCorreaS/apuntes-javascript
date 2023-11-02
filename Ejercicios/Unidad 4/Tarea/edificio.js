6510d3103b4985b69c71fcf39ff5bbc7a1c4723b
Cris Correa <cristina.correa.segade@gmail.com>
2023-11-02 14:37:34 +0100

    commit tarea unidad 4 planteada

---
+++ Ejercicios/Unidad 4/Tarea/edificio.js
@@ -0,0 +1,11 @@
+// Cristina Correa
+
+export default class Edificio {
+  constructor(calle, numero, codigoPostal, plantasEdificio) {
+    this.calle = calle;
+    this.numero = numero;
+    this.codigoPostal = codigoPostal;
+    this.plantasEdificio = plantasEdificio;
+  }
+}
+export { Edificio };
---
+++ Ejercicios/Unidad 4/Tarea/script.js
@@ -0,0 +1,7 @@
+// Cristina Correa
+
+/*
+Se puede hacer con clases o con arrays, si es con array sería:
+    edificioA[0][1] -> planta 0 puerta 1
+    ...
+*/
