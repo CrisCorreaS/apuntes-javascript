f6251f187f3abee268235162650b2cc09e0c8f0d
Cris Correa <cristina.correa.segade@gmail.com>
2023-11-10 13:40:19 +0100

    commit ejercicio 1 unidad 5

--- Ejercicios/Unidad 5/Ejercicio 01/script.js
+++ Ejercicios/Unidad 5/Ejercicio 01/script.js
@@ -7,4 +7,4 @@ document.addEventListener("mousemove", function (event) {
 
   // Actualiza el contenido del elemento con las nuevas coordenadas
   elementoRaton.textContent = `Posición del ratón: X: ${x}, Y: ${y}`; // "textContent" es como "innerHTML" con la diferencia de que, "textContent" devuelve Strings de texto que no pueden ser interpretadas como html, pero el texto de "innerHTML" sí que puede ser interpretado como html
-});
+}, false);
