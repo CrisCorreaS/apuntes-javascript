f33516cf0dd799f53cd0074846cd317f22a5748e
Cristina Correa <cristina.correa.segade@gmail.com>
2023-11-23 07:48:05 +0100

    commit ejercicio 9 unidad 5

--- Ejercicios/Unidad 5/Ejercicio 09/script.js
+++ Ejercicios/Unidad 5/Ejercicio 09/script.js
@@ -1,93 +1,141 @@
 document.getElementById("enviar").addEventListener("click", validar, false);
 
-function validarNombre(){
-    let elemento = document.getElementById("name");
+const NOMBRE_MENSAJE = document.getElementById("mensajeNombre");
+const CONTACTO_MENSAJE = document.getElementById("mensajeMovilCorreo");
+const CONTRASENA_MENSAJE = document.getElementById("mensajeContrasena");
+const CONTRASENA2_MENSAJE = document.getElementById("mensajeContrasena2");
 
-    if (!elemento.checkValidity()) {
-      if (elemento.validity.valueMissing) {
-        error(elemento, "Debe introducir un nombre y un apellido");
-      }
+// Miramos si existe el localStorage y si lo hace, los datos ya se guardan
+let nombreAlmacenado = localStorage.getItem("nombreUsuario");
+if (nombreAlmacenado) {
+  document.getElementById("name").value = nombreAlmacenado;
+}
 
-      if (elemento.validity.patternMismatch) {
-        error(elemento, "El nombre debe tener entre 1 y 40 caracteres");
-      }
+// Validación de campos
+function validarNombre() {
+  let elemento = document.getElementById("name");
 
-      return false;
+  if (!elemento.checkValidity()) {
+    if (elemento.validity.valueMissing) {
+      error(elemento, "<b>!</b> Introduce tu nombre");
     }
 
+    if (elemento.validity.patternMismatch) {
+      error(elemento, "<b>!</b> El nombre debe tener entre 1 y 40 caracteres");
+    }
+
+    return false;
+  }
+
+  return true;
+}
+
+function validarContacto() {
+  let regexTelefono = /^\d{9}$/;
+  let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+
+  let contacto = document.getElementById("contact");
+
+  // Así miramos que se cumplan las regex
+  if (regexMail.test(contacto.value) || regexTelefono.test(contacto.value)) {
     return true;
+  } else {
+    error(contacto, "<b>!</b> La dirección de correo electrónico o el número de teléfono móvil faltan o son inválidos. Corríjalo e inténtelo de nuevo.");
+    return false;
+  }
 }
 
-function validarContacto(){
+function validarContrasinal() {
+  let elemento = document.getElementById("password");
 
-}
-
-function validarContrasinal(){
-    let elemento = document.getElementById("password");
-
-    if (!elemento.checkValidity()) {
-      if (elemento.validity.valueMissing) {
-        error(elemento, "Debe introducir una contraseña");
-      }
-
-      if (elemento.validity.patternMismatch) {
-        error(elemento, "La contraseña debe tener al menos 6 caracteres");
-      }
-
-      return false;
+  if (!elemento.checkValidity()) {
+    if (elemento.validity.valueMissing) {
+      error(elemento, "<b>!</b> Introduce tu contraseña");
     }
 
+    if (elemento.validity.patternMismatch) {
+      error(elemento, "<b>!</b> La contraseña debe tener al menos 6 caracteres");
+    }
+
+    return false;
+  }
+
+  return true;
+}
+
+function validarContrasinales() {
+  let contrasinal = document.getElementById("password");
+  let contrasinal2 = document.getElementById("password2");
+
+  if (contrasinal.value === contrasinal2.value) {
     return true;
+  } else {
+    error(contrasinal2, "<b>!</b> Las dos contraseñas no coinciden");
+    return false;
+  }
 }
 
-function validarContrasinales(){
-    let contrasinal = document.getElementById("password");
-    let contrasinal2 = document.getElementById("password2");
+function validar(e) {
+  borrarError();
+  e.preventDefault();
 
-    if(contrasinal.value === contrasinal2.value){
-        return true;
-    } else{
-        error(contrasinal2, "Las dos contraseñas no coinciden");
-        return false;
-    }
+  // Así validamos todo al mismo tiempo en vez de ir uno a uno
+  let nombreValido = validarNombre();
+  let contactoValido = validarContacto();
+  let contrasenaValida = validarContrasinal();
+  let contraseniasValidas = validarContrasinales();
+
+  if (
+    nombreValido &&
+    contactoValido &&
+    contrasenaValida &&
+    contraseniasValidas
+  ) {
+    localStorage.setItem("nombreUsuario", document.getElementById("name").value); // Así creamos el localStorage que almacena el nombre
+    alert("El formulario se ha enviado");
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function validar(e){
-    borrarError();
-    if(validarNombre() && validarContacto() && validarContrasinal() && validarContrasinales()){
-        return true;
-    } else{
-        e.preventDefault();
-        return false;
-    }
+function error(elemento, mensaje) {
+  let idElemento = elemento.id;
+
+  elemento.className = "error";
+  elemento.focus();
+
+  switch (idElemento) {
+    case "name":
+      NOMBRE_MENSAJE.innerHTML = mensaje;
+      NOMBRE_MENSAJE.className = "mensajeError";
+      break;
+    case "contact":
+      CONTACTO_MENSAJE.innerHTML = mensaje;
+      CONTACTO_MENSAJE.className = "mensajeError";
+      break;
+    case "password":
+      CONTRASENA_MENSAJE.innerHTML = mensaje;
+      CONTRASENA_MENSAJE.className = "mensajeError";
+      break;
+    case "password2":
+      CONTRASENA2_MENSAJE.innerHTML = mensaje;
+      CONTRASENA2_MENSAJE.className = "mensajeError";
+      break;
+  }
 }
 
-function error(elemento, mensaje){
-    let idElemento = elemento.id;
+function borrarError() {
+  let formulario = document.forms[0];
+  let mensajes = [NOMBRE_MENSAJE, CONTACTO_MENSAJE, CONTRASENA_MENSAJE, CONTRASENA2_MENSAJE];
 
-    elemento.className = "error";
-    elemento.focus();
+  for (let i = 0; i < formulario.elements.length; i++) {
+    formulario.elements[i].className = "";
+  }
+  
+  for (let j = 0; j < mensajes.length; j++){
+    mensajes[j].className = "normal";
+    mensajes[j].innerHTML = "";
+  }
 
-    switch(idElemento){
-        case "name":
-            document.getElementById("mensajeNombre").innerHTML = mensaje;
-            break;
-        case "contact":
-            document.getElementById("mensajeMovilCorreo").innerHTML = mensaje;
-            break;
-        case "password":
-            document.getElementById("mensajeContrasena").innerHTML = mensaje;
-            break;
-        case "password2":
-            document.getElementById("mensajeContrasena2").innerHTML = mensaje;
-            break;
-    }
-}
-
-function borrarError(){
-    let formulario = document.forms[0];
-
-    for (let i = 0; i < formulario.elements.length; i++) {
-      formulario.elements[i].className = "";
-    }
 }
