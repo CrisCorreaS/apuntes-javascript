fde0b2622814e5ce6076cc2dc7dc00eb1bdb4e65
Cristina Correa <cristina.correa.segade@gmail.com>
2023-11-24 07:30:59 +0100

    commit ejercicio 11 unidad 5

---
+++ Ejercicios/Unidad 5/Ejercicio 11/script.js
@@ -0,0 +1,14 @@
+const BOTON = document.getElementById("miBoton");
+
+BOTON.addEventListener("mouseover", function () {
+  let coords = BOTON.getBoundingClientRect();
+
+  // Genera una nueva posición aleatoria para el botón
+  let nuevaPosicionTop = Math.floor(Math.random() * window.innerHeight);
+  let nuevaPosicionLeft = Math.floor(Math.random() * window.innerWidth);
+
+  // Aplica la nueva posición al botón
+  BOTON.style.position = "absolute";
+  BOTON.style.top = nuevaPosicionTop + "px";
+  BOTON.style.left = nuevaPosicionLeft + "px";
+});
