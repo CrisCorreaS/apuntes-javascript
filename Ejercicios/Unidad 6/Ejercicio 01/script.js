079390de21acbf69628d3503c15e5ad310ebd7ec
Cris Correa <cristina.correa.segade@gmail.com>
2024-01-10 14:39:47 +0100

    commit clase cliente

--- Ejercicios/Unidad 6/Ejercicio 01/script.js
+++ Ejercicios/Unidad 6/Ejercicio 01/script.js
@@ -1,8 +1,6 @@
-const FORMULARIO = document.getElementById("formulario");
 const INPUT = document.getElementById("tarea");
 const SUBMIT = document.getElementById("submit");
 const TEXTO = document.getElementById("texto");
-const LABELS = document.querySelectorAll("label");
 
 let tareas = [];
 let numCheckbox = 1;
@@ -11,11 +9,13 @@ let i = 0;
 SUBMIT.addEventListener("click", addTarea, false);
 
 function addTarea(e) {
-  let textoCheckbox = document.createTextNode(INPUT.value);
+  let textoCheckbox = INPUT.value;
 
   const nuevaTarea = { texto: textoCheckbox, realizada: false };
 
   tareas.push(nuevaTarea);
+
+  INPUT.value = "";
 
   e.preventDefault();
 
@@ -25,33 +25,28 @@ function addTarea(e) {
 function showTarea() {
   // Creamos los elementos y los nodos
   let checkbox = document.createElement("input");
-  let label = document.createElement("label");
-  let br = document.createElement("br");
+  let li = document.createElement("li");
+  let tareaTexto = tareas[i].texto;
+  let texto = document.createTextNode(tareaTexto);
 
-  // Creamos los atributos del input para que sea algo tipo: <input type="checkbox" id="1">
   checkbox.type = "checkbox";
   checkbox.id = numCheckbox.toString();
 
-  // Creamos los atributos del label y le añadimos todo lo necesario
-  label.for = numCheckbox.toString();
-
-  label.appendChild(checkbox);
-  label.appendChild(tareas[i].texto);
-  label.appendChild(br);
+  li.appendChild(checkbox);
+  li.appendChild(texto);
 
   // Le ponemos el addEventListener() al label
-  label.addEventListener(
-    "click", () => {
-      label.classList.toggle("incorrecto");
+  li.addEventListener(
+    "click",
+    () => {
+      li.classList.toggle("incorrecto");
     },
     false
   );
 
   // Ponemos el label dentro del div
-  TEXTO.appendChild(label);
+  TEXTO.appendChild(li);
 
   numCheckbox++;
   i++;
 }
-
-  
