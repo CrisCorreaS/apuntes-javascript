54d9b1b9a3308ac716c621a10aa27b26b6061d10
Cris Correa <cristina.correa.segade@gmail.com>
2024-01-23 11:24:57 +0100

    ejercicio 3 unidad 7 casi ok

---
+++ Ejercicios/Unidad 7/Ejercicio 03/script.js
@@ -0,0 +1,165 @@
+const CARTAS = document.querySelectorAll(".memory-card");
+const CARTA_IMG = document.querySelectorAll(".front-face");
+
+let personas = [];
+
+// Acceso y Gestión de los datos de la API
+function cargarXML(xml) {
+  let documentoXML = xml.responseXML;
+  let resultadosXML = documentoXML.getElementsByTagName("results");
+
+  for (let i = 0; i < resultadosXML.length - 1; i++) {
+    let imagenesXML = resultadosXML[i].getElementsByTagName("picture")[0];
+    let imagenXML = imagenesXML.getElementsByTagName("medium")[0].textContent;
+
+    let persona = {
+      imagen: imagenXML,
+      mail: resultadosXML[i].getElementsByTagName("email")[0].textContent,
+    };
+
+    personas.push(persona);
+  }
+
+  gestionImagenes();
+}
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    cargarXML(this);
+  }
+};
+xhr.open("GET", "https://randomuser.me/api/?results=6&format=XML", true);
+xhr.send();
+
+function gestionImagenes() {
+  CARTA_IMG.forEach((imagen) => {
+    let tipoimagen = imagen.getAttribute("data-value");
+
+    switch (tipoimagen) {
+      case "0":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "1":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "2":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "3":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "4":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "5":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+    }
+  });
+}
+
+// Código del 5.6
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let cont = 0;
+
+function flipCard(e) {
+  if (lockBoard) return;
+  if (e.target === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = e.target;
+
+    return;
+  }
+
+  secondCard = e.target;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.src === secondCard.src;
+
+  if (isMatch) {
+    disableCARTAS();
+    cont++;
+    document.getElementById("aciertos").innerHTML =
+      "Número de aciertos: " + cont;
+  } else {
+    unflipCARTAS();
+  }
+}
+
+function disableCARTAS() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCARTAS() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  CARTAS.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+CARTAS.forEach((card) => card.addEventListener("click", flipCard));
+
+// COOKIES:
+let nombre = getCookie("nombre");
+let visitas = getCookie("visitas");
+
+if (!nombre) {
+  nombre = prompt("Por favor, introduce tu nombre:");
+  document.cookie = "nombre=" + nombre + ";";
+}
+
+if (!visitas) {
+  visitas = 0;
+} else {
+  visitas++;
+}
+
+document.cookie = "visitas=" + visitas + ";";
+
+alert("Hola " + nombre + ", has visitado esta página " + visitas + " veces.");
+
+function getCookie(nombre) {
+  let nom = nombre + "=";
+  let array = document.cookie.split(/;/g);
+
+  for (var i = 0; i < array.length; i++) {
+    var c = array[i];
+
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(nombre) == 0) {
+      return c.substring(nom.length, c.length);
+    }
+  }
+  return "";
+}
