51ef495e8842f951d014c85170dbfd7372f2250a
Cristina Correa <cristina.correa.segade@gmail.com>
2024-01-31 11:50:42 +0100

    parte xml tarea unidad 7

--- Ejercicios/Unidad 7/Tarea/XML/script.js
+++ Ejercicios/Unidad 7/Tarea/XML/script.js
@@ -1,8 +1,138 @@
-let SELECT_FROM = document.querySelector("#from");
-let SELECT_TO = document.querySelector("#to");
+const SELECT_FROM = document.querySelector("#from");
+const SELECT_TO = document.querySelector("#to");
+const CHANGE = document.querySelector("i");
+const CONVERTIR = document.querySelector("#submit");
+const CANTIDAD = document.querySelector("#importe");
+const ERROR_MSG = document.querySelector("#errorMsg");
+const RESULTADO = document.querySelector("#resultado");
+const INFO = document.querySelector("#info");
 
 let monedas = [];
+let opcionFromSeleccionada = "1";
+let opcionToSeleccionada = "4";
 
+function error(elemento, mensaje) {
+  ERROR_MSG.innerHTML = mensaje;
+  ERROR_MSG.className = "errorMensaje";
+
+  elemento.className = "error";
+  elemento.focus();
+}
+
+function borrarError() {
+  let formulario = document.forms[0];
+
+  for (let k = 0; k < formulario.elements.length; k++) {
+    formulario.elements[k].className = "";
+  }
+
+  ERROR_MSG.innerHTML = " ";
+  ERROR_MSG.className = "";
+}
+
+function validarCantidad() {
+  if (!CANTIDAD.checkValidity()) {
+    if (CANTIDAD.validity.valueMissing) {
+      error(CANTIDAD, "Tienes que introducir una cantidad");
+    }
+
+    if (CANTIDAD.validity.rangeOverflow) {
+      error(CANTIDAD, "La cantidad no puede ser mayor de 1.000.000");
+    }
+
+    if (CANTIDAD.validity.rangeUnderflow) {
+      error(CANTIDAD, "La cantidad no puede ser menor de 0");
+    }
+
+    return false;
+  }
+
+  return true;
+}
+
+function calcularTasaCambio(e) {
+  e.preventDefault();
+  borrarError();
+  if (validarCantidad()) {
+    let cantidadACambiar = CANTIDAD.value;
+
+    let tasaCambio =
+      monedas[Number.parseInt(opcionFromSeleccionada) - 1].precio /
+      monedas[Number.parseInt(opcionToSeleccionada) - 1].precio;
+
+    RESULTADO.innerHTML = `${cantidadACambiar} 
+      ${monedas[Number.parseInt(opcionFromSeleccionada) - 1].siglas} son 
+      <b>${(cantidadACambiar * tasaCambio).toFixed(4)}</b>
+      ${monedas[Number.parseInt(opcionToSeleccionada) - 1].siglas}`;
+    
+    INFO.innerHTML = `1 
+      ${monedas[Number.parseInt(opcionFromSeleccionada) - 1].siglas} =
+      ${tasaCambio.toFixed(4)} 
+      ${monedas[Number.parseInt(opcionToSeleccionada) - 1].siglas}
+      <br/>
+      1 
+      ${monedas[Number.parseInt(opcionToSeleccionada) - 1].siglas} =
+      ${(1/tasaCambio).toFixed(4)} 
+      ${monedas[Number.parseInt(opcionFromSeleccionada) - 1].siglas}
+      `;
+  }
+}
+
+CONVERTIR.addEventListener("click", calcularTasaCambio, false);
+
+function crearSelectsOptions() {
+  let i = 1;
+
+  monedas.forEach((moneda) => {
+    let opcion = document.createElement("option");
+    let textoOpcion;
+
+    switch (moneda.siglas) {
+      case "USD":
+        textoOpcion = document.createTextNode(
+          `${moneda.siglas} (Dólares Estadounidenses)`
+        );
+        break;
+      case "AUD":
+        textoOpcion = document.createTextNode(
+          `${moneda.siglas} (Dólares Australianos)`
+        );
+        break;
+      case "CAD":
+        textoOpcion = document.createTextNode(
+          `${moneda.siglas} (Dólares Canadienses)`
+        );
+        break;
+      case "EUR":
+        textoOpcion = document.createTextNode(`${moneda.siglas} (Euros)`);
+        break;
+      case "GBP":
+        textoOpcion = document.createTextNode(
+          `${moneda.siglas} (Libra Esterlina)`
+        );
+        break;
+      case "JPY":
+        textoOpcion = document.createTextNode(
+          `${moneda.siglas} (Yenes Japoneses)`
+        );
+        break;
+    }
+
+    opcion.value = i;
+
+    opcion.appendChild(textoOpcion);
+    SELECT_FROM.appendChild(opcion);
+
+    let copiaOpcion = opcion.cloneNode(true);
+
+    if (i == 4) {
+      copiaOpcion.selected = "selected"; // Así hacemos que en el segundo select, los euros sean la opción por defecto
+    }
+    SELECT_TO.appendChild(copiaOpcion);
+
+    i++;
+  });
+}
 
 function cargarXML(xml) {
   let documentoXML = xml.responseXML;
@@ -18,7 +148,6 @@ function cargarXML(xml) {
       case "3":
       case "6":
       case "7":
-    
         let moneda = {
           siglas: resultadosXML[i].getElementsByTagName("Code")[0].textContent,
           precio: resultadosXML[i].getElementsByTagName("Ask")[0].textContent,
@@ -26,26 +155,9 @@ function cargarXML(xml) {
 
         monedas.push(moneda);
     }
-  };
+  }
 
-
-  // Crear Selects
-  let i = 1;
-  
-    monedas.forEach((moneda) => {
-      let opcion = documentoXML.createElement("option");
-      let textoOpcion = documentoXML.createTextNode(moneda.siglas);
-      
-
-      opcion.value = i;
-
-      opcion.appendChild(textoOpcion);
-      SELECT_FROM.appendChild(opcion);
-      SELECT_TO.appendChild(opcion);
-
-      i++;
-    });
-
+  crearSelectsOptions();
 }
 
 let xhr = new XMLHttpRequest();
@@ -60,3 +172,23 @@ xhr.open(
   true
 );
 xhr.send();
+
+SELECT_FROM.addEventListener("change", function () {
+  opcionFromSeleccionada = this.value;
+});
+
+SELECT_TO.addEventListener("change", function () {
+  opcionToSeleccionada = this.value;
+});
+
+
+function cambiar() {
+  SELECT_FROM.value = opcionToSeleccionada;
+  SELECT_TO.value = opcionFromSeleccionada;
+
+  let helper = opcionFromSeleccionada;
+  opcionFromSeleccionada = opcionToSeleccionada;
+  opcionToSeleccionada = helper;
+}
+
+CHANGE.addEventListener("click", cambiar, false);
