322c6e3329110d2c5366feb8b8602ee8909ede44
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-11 04:19:47 +0100

    Ejercicio 1 unidad 8

---
+++ Ejercicios/Unidad 8/Ejercicio 01/script.js
@@ -0,0 +1,170 @@
+const CARTAS = document.querySelectorAll(".memory-card");
+const CARTA_IMG = document.querySelectorAll(".front-face");
+const SPAN = document.querySelectorAll(".mail");
+
+let personas = [];
+let contador = 0;
+
+// Acceso y Gestión de los datos de la API
+function obtenerDatos() {
+  let persona;
+
+  $.ajax({
+    url: "https://randomuser.me/api/?results=6&format=XML",
+    dataType: "xml",
+    success: function (data) {
+      $(data)
+        .find("results")
+        .each(function () {
+          persona = {
+            imagen: $(this).find("picture").find("medium").text(),
+            mail: $(this).find("email").text(),
+          };
+
+          personas.push(persona);
+        });
+
+      // Llamada a gestionImagenes() tras completar el procesamiento de los datos -> Para que gestionImagenes() se ejecute solo después de que se complete la función obtenerDatos(), hay que llamar a gestionImagenes() dentro del bloque de éxito (success) de la petición AJAX en obtenerDatos()
+      gestionImagenes();
+    },
+  });
+
+  // Si pongo gestionImagenes() aquí, se haría antes de que acabase de procesarse toda la obtención de información de la api
+}
+
+obtenerDatos();
+
+function gestionImagenes() {
+  CARTA_IMG.forEach((imagen) => {
+    let tipoimagen = imagen.getAttribute("data-value");
+
+    switch (tipoimagen) {
+      case "0":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "1":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "2":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "3":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "4":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+      case "5":
+        imagen.src = personas[Number.parseInt(tipoimagen)].imagen;
+        break;
+    }
+    SPAN[contador].innerHTML = personas[Number.parseInt(tipoimagen)].mail;
+
+    contador++;
+  });
+}
+
+// Código del 5.6
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let cont = 0;
+
+function flipCard(e) {
+  if (lockBoard) return;
+  if (e.target.parentNode.firstElementChild === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = e.target.parentNode.firstElementChild;
+
+    return;
+  }
+
+  secondCard = e.target.parentNode.firstElementChild;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.src === secondCard.src;
+
+  if (isMatch) {
+    disableCARTAS();
+    cont++;
+    document.getElementById("aciertos").innerHTML =
+      "Número de aciertos: " + cont;
+  } else {
+    unflipCARTAS();
+  }
+}
+
+function disableCARTAS() {
+  firstCard.parentNode.removeEventListener("click", flipCard);
+  secondCard.parentNode.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCARTAS() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.parentNode.classList.remove("flip");
+    secondCard.parentNode.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  CARTAS.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+CARTAS.forEach((card) => card.addEventListener("click", flipCard));
+
+// COOKIES:
+let nombre = getCookie("nombre");
+let visitas = getCookie("visitas");
+
+if (!nombre) {
+  nombre = prompt("Por favor, introduce tu nombre:");
+  document.cookie = "nombre=" + nombre + ";";
+}
+
+if (!visitas) {
+  visitas = 0;
+} else {
+  visitas++;
+}
+
+document.cookie = "visitas=" + visitas + ";";
+
+alert("Hola " + nombre + ", has visitado esta página " + visitas + " veces.");
+
+function getCookie(nombre) {
+  let nom = nombre + "=";
+  let array = document.cookie.split(/;/g);
+
+  for (var i = 0; i < array.length; i++) {
+    var c = array[i];
+
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(nombre) == 0) {
+      return c.substring(nom.length, c.length);
+    }
+  }
+  return "";
+}
